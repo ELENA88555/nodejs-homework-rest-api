6d2d40794701b7e96cf92cca9c701882da418fda
Elena Us <Elena88555@gmail.com>
2023-07-13 23:36:04 +0300

    router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const ctrl = require('../../controllers/contacts')
+const ctrl = require("../../controllers/contacts");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById );
+router.get("/:id", ctrl.getById);
 
 router.post("/", ctrl.add);
 
-router.delete("/:id",ctrl.remove);
+router.delete("/:id", ctrl.remove);
 
 router.put("/:id", ctrl.updateById);
 
